34429d6164a75b8da47b219462d242a1c199491c
Alejandro F <afuste7@gmail.com>
2020-03-25 17:09:04 -0400

    added schema file

---
+++ db_schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name_depart VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE company_role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES company_role(id),
+    FOREIGN KEY (manager_id) REFERENCES company_role(id)
+);
+
+SELECT * FROM department;
+SELECT * FROM company_role;
+SELECT * FROM employee;
